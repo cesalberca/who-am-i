bd7e90a6613d09a839f0d34b98ad333ea50089c3
César Alberca <cesalberca@gmail.com>
2020-03-25 10:51:10 +0100

    Remove unused parameter

--- src/features/functions/src/index.ts
+++ src/features/functions/src/index.ts
@@ -2,30 +2,28 @@ import * as functions from 'firebase-functions'
 import { Assigner } from './assigner'
 import { Game } from '../../../core/game'
 
-export const onGameStart = functions.firestore
-  .document('games/{gameId}')
-  .onUpdate((change, _context) => {
-    const data = change.after.data() as Game | undefined
-    const previousData = change.before.data() as Game | undefined
+export const onGameStart = functions.firestore.document('games/{gameId}').onUpdate(change => {
+  const data = change.after.data() as Game | undefined
+  const previousData = change.before.data() as Game | undefined
 
-    if (
-      data?.start === undefined ||
-      previousData?.start !== undefined ||
-      (data?.players?.length ?? 0) < 3
-    ) {
-      return null
+  if (
+    data?.start === undefined ||
+    previousData?.start !== undefined ||
+    (data?.players?.length ?? 0) < 3
+  ) {
+    return null
+  }
+
+  const assigner = new Assigner({
+    provide(): number {
+      return Math.random()
     }
-
-    const assigner = new Assigner({
-      provide(): number {
-        return Math.random()
-      }
-    })
-
-    return change.after.ref.set(
-      {
-        assignees: assigner.assign(data.players)
-      },
-      { merge: true }
-    )
   })
+
+  return change.after.ref.set(
+    {
+      assignees: assigner.assign(data.players)
+    },
+    { merge: true }
+  )
+})
