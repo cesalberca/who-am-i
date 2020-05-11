fcb789e9ba583d4ffac7ed4995687854140597af
César Alberca <cesalberca@gmail.com>
2020-05-11 18:11:14 +0200

    Add share

--- src/core/use-case/links/executor-link.ts
+++ src/core/use-case/links/executor-link.ts
@@ -1,13 +1,13 @@
 import { BaseLink } from './base-link'
 import { Context } from '../context'
-import { shareReplay } from 'rxjs/operators'
+import { share } from 'rxjs/operators'
 
 export class ExecutorLink extends BaseLink {
   next(context: Context): void {
     if (!context.hasSetObservable) {
       const observable = context.useCase.internalExecute(context.param)
       if (context.useCase.readonly) {
-        context.observable = observable.pipe(shareReplay(1))
+        context.observable = observable.pipe(share())
       } else {
         context.observable = observable
       }
